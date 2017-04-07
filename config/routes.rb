43602c2461a463cece6c21acdf2c78fa225fab0c
Devin Leduc <ixeres@gmail.com>
2017-04-07 13:20:51 -0400

    Making each picture show at its own URL with dynamic routes. I almost understand what that means.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-Rails.application.routes.draw do
   get 'pictures' => 'pictures#index'
-
-  # Ignore the comments below for now
-  # They are just documentation
+  get 'pictures/:id' => 'pictures#show', as: 'picture'
 end
+
+# # Explaining it for myself.
+#
+# takes the 'pictures' instruction and directs the view to the pictures index in views/pictures/index
+#
+# takes the 'pictures/:id' instruction and implements the ruby code to display the appropriate picture.
+# These are examples of GETs, as in they GET information to display.
+# This is the Rails Router, which is directing the controller (PicturesController) to display the view (index).
+# The (index) is populated by information in the database that is interpreted by the model (picture.rb).
+
+#... I think that sounds right. 
