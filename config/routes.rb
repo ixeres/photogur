1ab1fd8f4d19397bb979d0046f370d2bef9b0eba
Devin Leduc <ixeres@gmail.com>
2017-04-07 13:39:48 -0400

    Adding feature to create new picture using HTML form. Yup. Those were some words.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'pictures' => 'pictures#index'
+
+  post 'pictures' => 'pictures#create' # this is a new line of code
+  get 'pictures/new' => 'pictures#new' # this is also a new line of code
+
   get 'pictures/:id' => 'pictures#show', as: 'picture'
 end
 
@@ -12,4 +16,11 @@ end
 # This is the Rails Router, which is directing the controller (PicturesController) to display the view (index).
 # The (index) is populated by information in the database that is interpreted by the model (picture.rb).
 
-#... I think that sounds right. 
+#... I think that sounds right.
+#
+# Writing my notes here seems to make sense, since the routes are kind of like the hub for all the comings and goings in the app.
+
+# The POST protocol tells the server that we are GIVING it information to present. So posting a new picture, in this case.
+# And then the subsequent GET displays the newly posted picture. Right?
+
+# Work in this order = RCAV, Routes Controllers, Actions, Views.
